2687e1bb81c659ada13965403f1b3abacb230c99
tlsgptj <hyeseoshin@DESKTOP-QO9E1VG>
2024-11-16 12:20:25 +0900

    urls.py 수정

--- emotionalDiary/urls.py
+++ emotionalDiary/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from Users import views
-from posts.views import CreateEmotionAPIView, EmotionListAPIView
-from Users.views import RegisterAPIView, LoginAPIView, LogoutAPIView
+from posts.views import CreateEmotionAPIView, EmotionListAPIView, EmotionViewSet
+from Users.views import RegisterAPIView, LoginAPIView, LogoutAPIView, UserViewSet
 
 # DRF의 DefaultRouter를 사용하여 뷰셋을 등록할 수 있습니다.
 router = DefaultRouter()
-router.register(r'Users', views.UserViewSet)
+router.register(r'Users', UserViewSet, basename='user')
+router.register(r'Emotions', EmotionViewSet, basename='emotion')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,4 +17,5 @@ urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),  
     path('login/', LoginAPIView.as_view(), name='login'),  
     path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('', include(router.urls)),
 ]
