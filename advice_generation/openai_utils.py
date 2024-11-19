33b3e8c629c1d8354a40843f1500d4c62f775e86
tlsgptj <hyeseoshin@DESKTOP-QO9E1VG>
2024-11-19 14:44:06 +0900

    openai 명령 삽입

--- advice_generation/openai_utils.py
+++ advice_generation/openai_utils.py
@@ -1,23 +1,37 @@
 from openai import OpenAI
-import config
+from advice_generation.config import config
 from models import Conversation
+import config
 
 client = OpenAI(api_key=config.OPEN_API_KEY)
 #명령을 입력하는 로직인것 같음
 system_instructions = """너는 감정상담사이고, 글을 쓰면 그 사람이 감동을 받을 수 있게 조언을 해주는 역할을 한다."""
 
-completion = client.chat.completions.create(
-    model = "gpt-4.0-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": system_instructions,
-        },
-        {
-            "role" : "user"
-            "content" : "Conversation.user_input"
-        },
-    ],
+def get_chat_response(user_input):
+    #챗봇답변
+    completion = client.chat.completions.create(
+        model="chatgpt-4o-latest",
+        messages=[
+            {
+                "role": "system",
+                "content": system_instructions,
+
+            },
+            {
+                "role": "user",
+                "content": user_input,
+            },
+        ],
+    )
+#이거 DB로 저장하는 로직으로 변경완료
+bot_response = get_chat_response.completion.choices[0].message.content
+#이게 맞는지 모르겟음 같은 completion인가?
+
+conversation = Conversation.objects.create(
+    user=Conversation.user,
+    user_input=Conversation.user_input,
+    bot_response=Conversation.bot_response
 )
-#이거 DB로 저장하는 로직으로 변경해야함
-#print(completion.choices[0].message.content)
+
+
+#config가 두개임, 근데 API키를 가리키고 있지는 않는 것 같음
--- emotionalDiary/settings.py
+++ emotionalDiary/settings.py
@@ -155,8 +155,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-OPEN_API_KEY = "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"
-
 
 
 # Static files (CSS, JavaScript, Images)
