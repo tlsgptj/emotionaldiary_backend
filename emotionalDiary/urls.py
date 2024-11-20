b7f414b786b126a289be572c92598b0d6e2d8c6b
tlsgptj <hyeseoshin@DESKTOP-QO9E1VG>
2024-11-20 19:55:12 +0900

    장소 가져오는 api 제작

--- emotionalDiary/urls.py
+++ emotionalDiary/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from posts.views import CreateEmotionAPIView, EmotionListAPIView, EmotionViewSet
 from Users.views import RegisterAPIView, LoginAPIView, LogoutAPIView, UserViewSet
-
+from wordCloud.views import GenerateWordCloudAPIView
 # DRF의 DefaultRouter를 사용하여 뷰셋을 등록할 수 있습니다.
 router = DefaultRouter()
 router.register(r'Users', UserViewSet, basename='user')
@@ -17,5 +17,6 @@ urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),  
     path('login/', LoginAPIView.as_view(), name='login'),  
     path('logout/', LogoutAPIView.as_view(), name='logout'),
+    path('generate_wordcloud/<int:user_id>/', GenerateWordCloudAPIView.as_view(), name='wordCloud'),
     path('', include(router.urls)),
 ]
--- locationServer/views.py
+++ locationServer/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
+from .models import Locations
 
-# Create your views here.
+@login_required
+def get_location_data(request):
+    if request.method == 'GET':
+        #장소 데이터 DB에서 가져옴
+        user = request.user
+        locations = Locations.objects.filter(user=user)
+        locations.values('user_Locations')#상황 이름 선택
+        locations.annotate(frequency=Count('user_Locations')) #필요하다면 빈도수
+        locations.order_by('-frequency')[:4]
+
+        situation_data = [
+            {
+                'user_Locations': Locations['user_Locations'],
+                'frequency': Locations['frequency'],
+            }
+            for situation in situation_data
+        ]
+
+        return JsonResponse({'top_situations' : situation_data}, safe=False)
+    
+    return JsonResponse({"error" : "Invalid request method"}, status=405)
+
+
